85dfa14b4c0a8dfc703624a02de5662736b9b0ef
Ramandeep Singh <raman19930@gmail.com>
2021-11-29 12:30:00 +0530

    Source Code upload

---
+++ DevTask/Assets/Scripts/LevelGenerater.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerater : MonoBehaviour {
+
+	public Texture2D map;
+	public Color enemyColor = new Color(0, 1, 1, 1);
+	public int enemyCount, count;
+	public GameObject enemy;
+	public ColorToPrefab[] colorMappings;
+	public List<Vector2> spawnPoints;
+	// Use this for initialization
+	void Start () {
+		GenerateLevel();
+		Debug.Log("Game Started!");
+	}
+
+	void GenerateLevel()
+	{	
+		for(int x =0; x < map.width; x++)
+		{
+			for (int y = 0; y < map.height; y++)
+			{
+				GenerateTile(x, y);
+
+			}
+		}
+		GenerateEnemy();
+	}
+
+	//Method responsible to Generate Walls at x,y points, denoted as black, red & blue in the 2d map.
+	void GenerateTile(int x, int y)
+	{
+		Color pixelColor = map.GetPixel(x, y);
+		//Debug.Log("Position" + x + ", " + y + " Pixel color" + pixelColor);
+		if (pixelColor.a == 0)
+		{
+			return;
+		}
+		if (pixelColor == enemyColor)
+		{
+			spawnPoints.Add(new Vector2(x, y));
+		}
+		else
+		{
+			foreach (ColorToPrefab colorMapping in colorMappings)
+			{
+
+				if (colorMapping.color.Equals(pixelColor))
+				{
+					Vector3 position = new Vector3(x - 16, .2f, y - 16);
+					Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+				}
+			}
+		}
+	}
+	//Method responsible to Generate Enemy at random spawn points, denoted as teal in the 2d map.
+	void GenerateEnemy()
+	{
+		int Listsize = spawnPoints.Count;
+		int[] spawned = new int[Listsize];
+
+		for (; count < enemyCount; count++)
+		{
+			int position = Random.Range(0, Listsize);
+			Vector3 pos = new Vector3(spawnPoints[position].x-16, 2, spawnPoints[position].y-16);
+			Instantiate(enemy, pos, Quaternion.identity, transform);
+			spawned[position] = 1;
+		}
+	}
+
+}
---
+++ DevTask/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed = 12f;
+	public bool isPlayer = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		if (!isPlayer)
+		{
+			speed = -speed;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Rigidbody rb = GetComponent<Rigidbody>();
+
+		if (Input.GetKey(KeyCode.A))
+			rb.AddForce(Vector3.left * speed);
+		if (Input.GetKey(KeyCode.D))
+			rb.AddForce(Vector3.right * speed);
+		if (Input.GetKey(KeyCode.W))
+			rb.AddForce(Vector3.forward * speed);
+		if (Input.GetKey(KeyCode.S))
+			rb.AddForce(Vector3.back * speed);
+
+	}
+
+	// Method to handle the collision
+	private void OnCollisionEnter(Collision collision)
+	{
+		Collider cb = GetComponent<Collider>();
+		if (collision.gameObject.tag == "BlueWall" && isPlayer)
+		{
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), cb);
+		}
+		if (collision.gameObject.tag == "RedWall" && !isPlayer)
+		{
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), cb);
+		}
+		if (collision.gameObject.tag == "Enemy" && isPlayer)
+		{
+			Debug.Log("LOST");
+			this.gameObject.SetActive(false);
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+		}
+		if (collision.gameObject.tag == "Win" && isPlayer)
+		{
+			Debug.Log("WON");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+		}
+	}
+
+}
